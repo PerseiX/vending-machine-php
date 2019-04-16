6afdbbe6f25c7428a96a831486ed2595f13e225b
Kamil <kamilrytel2@gmail.com>
2019-04-16 18:24:52 +0000

    vending machine

--- src/VendingMachine/VendingMachine.php
+++ src/VendingMachine/VendingMachine.php
@@ -1,14 +1,95 @@
 <?php
 
-declare(strict_types=1);
 
 namespace VendingMachine;
 
-final class VendingMachine {
-    function __construct() {}
 
-    public function service(): string
+class VendingMachine
+{
+    /**
+     * @var Wallet
+     */
+    private $internalWallet;
+
+    /**
+     * @var Wallet
+     */
+    private $userWallet;
+
+    /**
+     * @var array
+     */
+    private $items;
+
+    public function setInternalWallet(Wallet $wallet)
     {
-        return 'no service';
+        $this->internalWallet = $wallet;
+        return $this;
     }
+
+    /**
+     * @param Wallet $userWallet
+     * @return $this
+     */
+    public function setUserWallet(Wallet $userWallet)
+    {
+        $this->userWallet = $userWallet;
+        return $this;
+    }
+
+    /**
+     * @param array $items
+     * @return $this
+     */
+    public function setItems(array $items)
+    {
+        $this->items = $items;
+
+        return $this;
+    }
+
+    /**
+     * @return Wallet
+     */
+    public function getInternalWallet()
+    {
+        return $this->internalWallet;
+    }
+
+    /**
+     * @return Wallet
+     */
+    public function getUserWallet()
+    {
+        return $this->userWallet;
+    }
+
+    /**
+     * @return array
+     */
+    public function getItems()
+    {
+        return $this->items;
+    }
+
+    public function giveProduct(Item $requiredItem)
+    {
+        foreach ($this->items as $item) {
+            if ($item->getKey() === $requiredItem->getKey()) {
+                if ($this->userWallet->getSum() === $requiredItem->getValue()) {
+                    return $requiredItem->getKey();
+                }
+                if ($this->userWallet->getSum() > $requiredItem->getValue()) {
+
+                    return $this->userWallet->getSum() - $requiredItem->getValue() . ' ' . $requiredItem->getKey();
+                }
+
+                return (string)$this->userWallet->getSum();
+            }
+        }
+
+        return (string)$this->userWallet->getSum();
+    }
+
+
 }
